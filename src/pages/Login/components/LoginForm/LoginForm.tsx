ab32d6dac09594de5c89739a19e479075af304b0
Luc Vu <lucvuit@gmail.com>
2023-02-02 10:05:17 +0700

    add account test

--- src/pages/Login/components/LoginForm/LoginForm.tsx
+++ src/pages/Login/components/LoginForm/LoginForm.tsx
@@ -1,45 +1,60 @@
 import { LockOutlined, UserOutlined } from "@ant-design/icons";
-import { Button, Form, Input } from "antd";
+import { Alert, Button, Form, Input } from "antd";
 import React from "react";
 import useLoginForm from "./hooks/useLoginForm";
-import './LoginForm.scss'
+import "./LoginForm.scss";
 
 const LoginForm = () => {
-  const {onLogin, loading} = useLoginForm()
+  const { onLogin, loading } = useLoginForm();
 
   return (
-    <Form name="login-form" className="login-form" onFinish={onLogin}>
-      <Form.Item
-        name="email"
-        rules={[{ required: true, message: "Please input your Username!" }]}
-        wrapperCol={{span: 24}}
-      >
-        <Input
-          prefix={<UserOutlined className="site-form-item-icon" />}
-          placeholder="Username"
-          size="large"
-          disabled = {loading}
-        />
-      </Form.Item>
-      <Form.Item
-        wrapperCol={{span: 24}}
-        name="password"
-        rules={[{ required: true, message: "Please input your Password!" }]}
-      >
-        <Input
-          prefix={<LockOutlined className="site-form-item-icon" />}
-          type="password"
-          placeholder="Password"
-          size="large"
-          disabled={loading}
-        />
-      </Form.Item>
-      <Form.Item className="button-field" wrapperCol={{span: 24}}>
-        <Button loading={loading} type="primary" size="large" htmlType="submit" className="login-form-button">
-          LOG IN
-        </Button>
-      </Form.Item>
-    </Form>
+    <>
+    <Alert
+      message="Account Test"
+      description="Username: test@test.com/Password: 123456"
+      type="info"
+      showIcon
+      style={{marginBottom: "24px"}}
+    />
+      <Form name="login-form" className="login-form" onFinish={onLogin}>
+        <Form.Item
+          name="email"
+          rules={[{ required: true, message: "Please input your Username!" }]}
+          wrapperCol={{ span: 24 }}
+        >
+          <Input
+            prefix={<UserOutlined className="site-form-item-icon" />}
+            placeholder="Username"
+            size="large"
+            disabled={loading}
+          />
+        </Form.Item>
+        <Form.Item
+          wrapperCol={{ span: 24 }}
+          name="password"
+          rules={[{ required: true, message: "Please input your Password!" }]}
+        >
+          <Input
+            prefix={<LockOutlined className="site-form-item-icon" />}
+            type="password"
+            placeholder="Password"
+            size="large"
+            disabled={loading}
+          />
+        </Form.Item>
+        <Form.Item className="button-field" wrapperCol={{ span: 24 }}>
+          <Button
+            loading={loading}
+            type="primary"
+            size="large"
+            htmlType="submit"
+            className="login-form-button"
+          >
+            LOG IN
+          </Button>
+        </Form.Item>
+      </Form>
+    </>
   );
 };
 
